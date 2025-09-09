b017b3fbea4a66fa7a41132b0ed9ef4fb52f49c6
Winlere <winlere@qq.com>
2025-09-09 13:03:57 -0500

    feat: add botons

--- src/js/index.js
+++ src/js/index.js
@@ -19,3 +19,32 @@ button3.addEventListener("click", () => {
   document.getElementsByClassName("container")[0].style.background =
     "linear-gradient(to bottom right, rgb(224, 234, 235), rgb(130, 204, 228))";
 });
+
+const profile_picture = document.getElementsByClassName("profile-image")[0];
+let current_mode = 0;
+
+profile_picture.addEventListener("click", () => {
+  // hide all bio. dont use forEach because it will return an array
+  // store the current mode as 0 if it is 0, show all bio
+
+  if (current_mode === 0) {
+    array = document.getElementsByClassName("bio");
+    for (let i = 0; i < array.length; i++) {
+      array[i].style.display = "none";
+    }
+    email = document.getElementsByClassName("email")[0];
+    email.style.display = "none";
+    github = document.getElementsByClassName("github")[0];
+    github.style.display = "none";
+    current_mode = 1;
+  } else {
+    array = document.getElementsByClassName("bio");
+    for (let i = 0; i < array.length; i++) {
+      // reset the display
+      array[i].style.display = "block";
+    }
+    email.style.display = "block";
+    github.style.display = "block";
+    current_mode = 0;
+  }
+});
