cc0eeb60408a667e5f0e846908cdd50d4e7f09b4
Winlere <winlere@qq.com>
2025-09-09 12:54:28 -0500

    feat: add bottons

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,21 @@
 /* Your JS here. */
 console.log('Hello World!')
+
+const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
+const button3 = document.getElementById("button3");
+
+button1.addEventListener("click", () => {
+  document.getElementsByClassName("container")[0].style.background =
+    "linear-gradient(to bottom right, rgb(130, 204, 228), rgb(224, 234, 235))";
+});
+
+button2.addEventListener("click", () => {
+  document.getElementsByClassName("container")[0].style.background =
+    "linear-gradient(to bottom right, rgb(169, 237, 169), rgb(224, 234, 235))";
+});
+
+button3.addEventListener("click", () => {
+  document.getElementsByClassName("container")[0].style.background =
+    "linear-gradient(to bottom right, rgb(224, 234, 235), rgb(130, 204, 228))";
+});
